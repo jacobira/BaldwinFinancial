fbe6490829bf3f3df196f4c60f82e5e5cfa49ba0
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-08-23 15:16:05 -0600

    Creation of navbar-button component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarButtonComponent } from './components/navbar/navbar-button/navbar-button.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent, data: {title: 'Home'} }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent,
+    NavbarButtonComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    ),
+    BrowserModule,
+    Routes
   ],
   providers: [],
   bootstrap: [AppComponent]
